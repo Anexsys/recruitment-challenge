25c6ec019dd749ea92d77aadfca5f0fcd3fceb89
Joe Shepherd <joe.shepherd@anexsys.com>
2018-12-06 15:47:26 +0000

    updated code - cleaned up references

--- CSharp/CSharp/Card.cs
+++ CSharp/CSharp/Card.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp
 {
-     class Card
+    class Card
     {
         public Suit? Suit { get; }
         public Rank? Rank { get; }
